9af08fa41c40da74dcfa7c2354cb16b9829b4a3b
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-03-24 14:43:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,3 +14,8 @@ function addElementToEndOfArray(array, element) {
 
   return [...array, element];
 }
+
+function destructivelyAddElementToEndOfArray(array, element) {
+array.push(element);
+  return array;
+}
